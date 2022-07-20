65a212ec5e38b21792bcfb735e95d6eae6d27810
dingo sky <paul@dingosky.com>
2022-07-20 13:25:56 -0600

    Use encoder to select chars encoder for testing

--- src/lib/encoder.spec.ts
+++ src/lib/encoder.spec.ts
@@ -1,40 +1,28 @@
 import test, { ExecutionContext } from 'ava'
 
 import { Chars } from './chars'
-import alphaEncoder from './encoder/alpha'
-import alphaCaseEncoder from './encoder/alphaCase'
-import alphaNumEncoder from './encoder/alphaNum'
-import alphaNumCaseEncoder from './encoder/alphaNumCase'
-import base32Encoder from './encoder/base32'
-import base32HexCaseEncoder from './encoder/base32HexCase'
-import decimalEncoder from './encoder/decimal'
-import hexCaseEncoder from './encoder/hexCase'
-import safe32Encoder from './encoder/safe32'
-import safe64Encoder from './encoder/safe64'
-import safeAsciiEncoder from './encoder/safeAscii'
-import symbolEncoder from './encoder/symbol'
+import encoder from './encoder'
 
-type PuidEncoder = (n: number) => number
-
-const encoder = (t: ExecutionContext, encoder: PuidEncoder, chars: string) => {
-  const codes = [...Array(chars.length).keys()].map(code => encoder(code))
+const chars_encoder = (t: ExecutionContext, chars: string) => {
+  const chars_encoder = encoder(chars)
+  const codes = [...Array(chars.length).keys()].map(code => chars_encoder(code))
   const encoded = String.fromCharCode(...codes)
   t.is(encoded, chars)
 }
 
-test('alpha() encoder chars', (t) => encoder(t, alphaEncoder(), Chars.Alpha))  
-test('alphaCase() encoder chars', (t) => encoder(t, alphaCaseEncoder(), Chars.AlphaLower))
-test('alphaCase(true) encoder chars', (t) => encoder(t, alphaCaseEncoder(true), Chars.AlphaUpper))
-test('alphaNumEncoder() encoder chars', (t) => encoder(t, alphaNumEncoder(), Chars.AlphaNum))
-test('alphaNumCaseEncoder() encoder chars', (t) => encoder(t, alphaNumCaseEncoder(), Chars.AlphaNumLower))
-test('alphaNumCaseEncoder(true) encoder chars', (t) => encoder(t, alphaNumCaseEncoder(true), Chars.AlphaNumUpper))
-test('base32Encoder() encoder chars', (t) => encoder(t, base32Encoder(), Chars.Base32))
-test('base32HexCaseEncoder() encoder chars', (t) => encoder(t, base32HexCaseEncoder(), Chars.Base32Hex))
-test('base32HexCaseEncoder(true) encoder chars', (t) => encoder(t, base32HexCaseEncoder(true), Chars.Base32HexUpper))
-test('decimalEncoder() encoder chars', (t) => encoder(t, decimalEncoder(), Chars.Decimal))
-test('hexCaseEncoder() encoder chars', (t) => encoder(t, hexCaseEncoder(), Chars.Hex))
-test('hexCaseEncoder(true) encoder chars', (t) => encoder(t, hexCaseEncoder(true), Chars.HexUpper))
-test('safeAsciiEncoder() encoder chars', (t) => encoder(t, safeAsciiEncoder(), Chars.SafeAscii))
-test('safe32Encoder() encoder chars', (t) => encoder(t, safe32Encoder(), Chars.Safe32))
-test('safe64Encoder encoder chars', (t) => encoder(t, safe64Encoder(), Chars.Safe64))
-test('symbolEncoder() encoder chars', (t) => encoder(t, symbolEncoder(), Chars.Symbol))
+test('alpha() encoder chars', (t) => chars_encoder(t, Chars.Alpha))  
+test('alphaCase() encoder chars', (t) => chars_encoder(t, Chars.AlphaLower))
+test('alphaCase(true) encoder chars', (t) => chars_encoder(t, Chars.AlphaUpper))
+test('alphaNumEncoder() encoder chars', (t) => chars_encoder(t, Chars.AlphaNum))
+test('alphaNumCaseEncoder() encoder chars', (t) => chars_encoder(t, Chars.AlphaNumLower))
+test('alphaNumCaseEncoder(true) encoder chars', (t) => chars_encoder(t, Chars.AlphaNumUpper))
+test('base32Encoder() encoder chars', (t) => chars_encoder(t, Chars.Base32))
+test('base32HexCaseEncoder() encoder chars', (t) => chars_encoder(t, Chars.Base32Hex))
+test('base32HexCaseEncoder(true) encoder chars', (t) => chars_encoder(t, Chars.Base32HexUpper))
+test('decimalEncoder() encoder chars', (t) => chars_encoder(t, Chars.Decimal))
+test('hexCaseEncoder() encoder chars', (t) => chars_encoder(t, Chars.Hex))
+test('hexCaseEncoder(true) encoder chars', (t) => chars_encoder(t, Chars.HexUpper))
+test('safeAsciiEncoder() encoder chars', (t) => chars_encoder(t, Chars.SafeAscii))
+test('safe32Encoder() encoder chars', (t) => chars_encoder(t, Chars.Safe32))
+test('safe64Encoder encoder chars', (t) => chars_encoder(t, Chars.Safe64))
+test('symbolEncoder() encoder chars', (t) => chars_encoder(t, Chars.Symbol))
--- src/lib/encoder.ts
+++ src/lib/encoder.ts
@@ -26,10 +26,13 @@ import alphaCaseEncoder from './encoder/alphaCase'
 import alphaNumEncoder from './encoder/alphaNum'
 import base32Encoder from './encoder/base32'
 import base32HexCaseEncoder from './encoder/base32HexCase'
+import customEncoder from './encoder/custom'
+import decimalEncoder from './encoder/decimal'
 import hexCaseEncoder from './encoder/hexCase'
 import safe32Encoder from './encoder/safe32'
 import safe64Encoder from './encoder/safe64'
 import safeAsciiEncoder from './encoder/safeAscii'
+import symbolEncoder from './encoder/symbol'
 
 export default (chars: string): PuidEncoder => {
   if (chars === Chars.Alpha) return alphaEncoder()
@@ -39,11 +42,13 @@ export default (chars: string): PuidEncoder => {
   if (chars === Chars.Base32) return base32Encoder()
   if (chars === Chars.Base32Hex) return base32HexCaseEncoder()
   if (chars === Chars.Base32HexUpper) return base32HexCaseEncoder(true)
+  if (chars === Chars.Decimal) return decimalEncoder()  
   if (chars === Chars.Hex) return hexCaseEncoder()
   if (chars === Chars.HexUpper) return hexCaseEncoder(true)
   if (chars === Chars.Safe32) return safe32Encoder()
   if (chars === Chars.Safe64) return safe64Encoder()
   if (chars === Chars.SafeAscii) return safeAsciiEncoder()
+  if (chars === Chars.Symbol) return symbolEncoder()
 
-  return customCharsEncoder(chars)
+  return customEncoder(chars)
 }
